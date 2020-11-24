a0b2eb2719708669ca4f0f8067234109bdb76c06
sjimpy <sjimpy@users.noreply.github.com>
2020-11-24 20:47:30 +0100

    Add files via upload
    Eerste sprint van coderen.
    Gemaakt:
    * Nav - responsive.
    * mobiele nav + animaties
    * Hover animaties.
    * eerste pagina begin.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+var burgerBTN = document.querySelector("nav .burger");
+var expand = document.querySelector("nav .expand");
+
+console.log(burgerBTN);
+
+burgerBTN.addEventListener("click", function() {
+  for (var i = 0; i < burgerBTN.querySelectorAll("span").length; i++) {
+    burgerBTN.querySelectorAll("span")[i].classList.toggle("clicked");
+  }
+  expand.classList.toggle("clicked");
+})
