a5895312c673cfc70bf32769d98102ef9b50d130
sjimpy <sjimpy@users.noreply.github.com>
2020-12-14 21:52:05 +0100

    added desktop footer, not responsive yet

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,26 @@
 var burgerBTN = document.querySelector("nav .burger");
 var expand = document.querySelector("nav .expand");
 
-console.log(burgerBTN);
-
 burgerBTN.addEventListener("click", function() {
   for (var i = 0; i < burgerBTN.querySelectorAll("span").length; i++) {
     burgerBTN.querySelectorAll("span")[i].classList.toggle("clicked");
   }
   expand.classList.toggle("clicked");
 })
+
+
+
+
+
+
+
+// var footerh3 = document.querySelector("footer");
+var footerh3 = document.querySelector("footer").querySelectorAll("h3");
+console.log(footerh3);
+
+for (var i = 0; i < footerh3.length; i++) {
+  footerh3[i].addEventListener("click", function() {
+    this.querySelector("span").toggle
+    console.log(this.querySelector("span"));
+  })
+}
